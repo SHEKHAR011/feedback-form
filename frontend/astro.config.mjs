ec0e17906d6371c90d4c83af3133e3f65ab11484
SHEKHAR <shekhar3203@gmail.com>
2025-11-08 17:33:58 +0530

    Lenis js Added

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,9 +4,11 @@ import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 import clerk from '@clerk/astro';
 
+import lenis from 'astro-lenis';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), clerk()],
+  integrations: [tailwind(), clerk(), lenis()],
   adapter: node({ mode: 'standalone' }),
   output: 'server',
 });
